b7acd99e39cde6795d8c8d6f18b4c0e00f3162d4
Glen Wasserfall <glen@wasserfalls.co.za>
2022-11-18 22:55:25 +0200

    up

--- check.py
+++ check.py
@@ -38,5 +38,4 @@ if len(payload) > 2:
     requests.get(f"""https://push.techulus.com/api/v1/notify/{push_hash}?title=Slots Available!&body=Quickly reserve a slot for Capetown!!""")
 
 else:
-    requests.get(f"""https://push.techulus.com/api/v1/notify/{push_hash}?title=Test!&body=Big test""")
     print(f"{datetime.now()} : No slots...")
